a688dba609d143385428e2c507fd268b7e031bd7
SeungaLeeah <92766716+SeungaLeeah@users.noreply.github.com>
2022-10-29 01:33:46 +0900

    codingtest

--- section3_문자열탐색/5.문자열압축.js
+++ section3_문자열탐색/5.문자열압축.js
@@ -1,15 +1,6 @@
 function solution(s){
-    let answer="";
-    let cnt=1;
-    s=s+" ";
-    for(let i=0; i<s.length-1; i++){
-        if(s[i]===s[i+1]) cnt++;
-        else{
-            answer+=s[i];
-            if(cnt>1) answer+=String(cnt);
-            cnt=1;
-        }
-    }
+    let answer;
+    
     return answer;
 }
 
